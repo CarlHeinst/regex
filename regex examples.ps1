f68ac46fdbbee3a796f1777d0f399b8585f5b9a2
CarlHeinst <cheinst@gmail.com>
2016-07-12 11:46:50 +0100

    Version 1.01

--- regex examples.ps1
+++ regex examples.ps1
@@ -63,6 +63,12 @@ $ip = "^10\.\d{1,3}\.10\.\d{1,3}$"
 "10.100.10.1","10.192.20.1","10.150.10.1","10.100.10.3","10.100.10.4","10.200.10.1" | where {$_ -match $ip}
 ## match all IPs 10.X.10.X
 
+"Bob,Imperial road, Imperial palace, Coruscant, CO15 8JH"
+
+"HEllo" -match "(?i)hello"  ### RESULT: TRUE (?i) starts case-insensitive mode
+"HEllo" -match "(?-i)hello" ### RESULT: FALSE (?-i) turns off case-insensitive mode
+
+"Bob,Bob Road,Bob Town,Land of bob" -replace "(?i)^(.*),(.*),(.*),(.*)$", "`$1" ## RESULT: BOB - this defines a pattern of three commas seperating 4 values and returns the first value
 
 
 $p = "^\\\\[Cc][Hh][Ii]-\w+\d{2}\\\w+$" ## [Cc] look for upper or lower case C or c
