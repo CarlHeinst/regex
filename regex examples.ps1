ff40f53cdda6faec104f14844e0ccd109b0a7b0b
CarlHeinst <cheinst@gmail.com>
2016-09-06 15:33:48 +0100

    Version 1.05

--- regex examples.ps1
+++ regex examples.ps1
@@ -106,3 +106,9 @@ $tickets | where {$_.subject -match $match} | select id,subject,@{n='INC';e={$ma
 
 $grabs = "123456789P","CCCC123456789C","UUU123456789U","123456789Z","123456789U","123456789u"
 $grabs -match "^\w{9}[Tt|Dd|Uu]" #Matches the 10th character as T, D or U
+
+
+$gioserv = "server12345.gio.com"
+[regex]$firstword = "\d+(?=\.)" ## Match and return any number, until you hit a . character
+$firstword.Match($gioserv)
+$firstword.Match($gioserv).Value
